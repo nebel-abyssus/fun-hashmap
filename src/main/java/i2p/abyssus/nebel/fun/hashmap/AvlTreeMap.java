6a4667d4f0851a1a8f8f32778f77a1d8e8317023
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-02-07 17:35:54 +0800

    Добавляем каркас класса AvlTreeMap

---
+++ src/main/java/i2p/abyssus/nebel/fun/hashmap/AvlTreeMap.java
@@ -0,0 +1,405 @@
+package i2p.abyssus.nebel.fun.hashmap;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class AvlTreeMap <K, V> implements NavigableMap<K, V> {
+
+// instance methods
+
+	@Override
+	public int size (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // size()
+
+	@Override
+	public boolean isEmpty (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // isEmpty()
+
+	@Override
+	public boolean containsKey (
+		final Object key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // containsKey()
+
+	@Override
+	public boolean containsValue (
+		final Object value
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // containsValue()
+
+	@Override
+	public V get (
+		final Object key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // get()
+
+	@Override
+	public V put (
+		final K key,
+		final V value
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // put()
+
+	@Override
+	public V remove (
+		final Object key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // remove()
+
+	@Override
+	public void putAll (
+		final Map<? extends K, ? extends V> m
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // putAll()
+
+	@Override
+	public void clear (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // clear()
+
+	@Override
+	public Set<K> keySet (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // keySet()
+
+	@Override
+	public Collection<V> values (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // values()
+
+	@Override
+	public Set<Entry<K, V>> entrySet (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // entrySet()
+
+	@Override
+	public boolean equals (
+		final Object obj
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // equals()
+
+	@Override
+	public int hashCode (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // hashCode()
+
+	@Override
+	public V getOrDefault (
+		final Object key,
+		final V defaultValue
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // getOrDefault()
+
+	@Override
+	public V putIfAbsent (
+		final K key,
+		final V value
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // putIfAbsent()
+
+	@Override
+	public boolean remove (
+		final Object key,
+		final Object value
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // remove()
+
+	@Override
+	public boolean replace (
+		final K key,
+		final V oldValue,
+		final V newValue
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // replace()
+
+	@Override
+	public V replace (
+		final K key,
+		final V value
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // replace()
+
+	@Override
+	public V computeIfAbsent (
+		final K key,
+		final Function<? super K, ? extends V> mappingFunction
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // computeIfAbsent()
+
+	@Override
+	public V computeIfPresent (
+		final K key,
+		final BiFunction<? super K, ? super V, ? extends V> remappingFunction
+	) throws NullPointerException
+	{ // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // computeIfPresent()
+
+	@Override
+	public V compute (
+		final K key,
+		final BiFunction<? super K, ? super V, ? extends V> remappingFunction
+	) throws NullPointerException
+	{ // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // compute()
+
+	@Override
+	public V merge (
+		final K key,
+		final V value,
+		final BiFunction<? super V, ? super V, ? extends V> remappingFunction
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // merge()
+
+	@Override
+	public NavigableMap<K, V> reversed (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // reversed()
+
+	@Override
+	public Entry<K, V> firstEntry (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // firstEntry()
+
+	@Override
+	public Entry<K, V> lastEntry (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // lastEntry()
+
+	@Override
+	public Entry<K, V> pollFirstEntry (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // pollFirstEntry()
+
+	@Override
+	public Entry<K, V> pollLastEntry (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // pollLastEntry()
+
+	@Override
+	public Comparator<? super K> comparator (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // comparator()
+
+	@Override
+	public NavigableMap<K, V> subMap (
+		final K fromKey,
+		final K toKey
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // subMap()
+
+	@Override
+	public NavigableMap<K, V> headMap (
+		final K toKey
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // headMap()
+
+	@Override
+	public NavigableMap<K, V> tailMap (
+		final K fromKey
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // tailMap()
+
+	@Override
+	public K firstKey (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // firstKey()
+
+	@Override
+	public K lastKey (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // lastKey()
+
+	@Override
+	public Entry<K, V> lowerEntry (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // lowerEntry()
+
+	@Override
+	public K lowerKey (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // lowerKey()
+
+	@Override
+	public Entry<K, V> floorEntry (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // floorEntry()
+
+	@Override
+	public K floorKey (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // floorKey()
+
+	@Override
+	public Entry<K, V> ceilingEntry (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // ceilingEntry()
+
+	@Override
+	public K ceilingKey (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // ceilingKey()
+
+	@Override
+	public Entry<K, V> higherEntry (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // higherEntry()
+
+	@Override
+	public K higherKey (
+		final K key
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // higherKey()
+
+	@Override
+	public NavigableMap<K, V> descendingMap (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // descendingMap()
+
+	@Override
+	public NavigableSet<K> navigableKeySet (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // navigableKeySet()
+
+	@Override
+	public NavigableSet<K> descendingKeySet (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // descendingKeySet()
+
+	@Override
+	public NavigableMap<K, V> subMap (
+		final K fromKey,
+		final boolean fromInclusive,
+		final K toKey,
+		final boolean toInclusive
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // subMap()
+
+	@Override
+	public NavigableMap<K, V> headMap (
+		final K toKey,
+		final boolean inclusive
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // headMap()
+
+	@Override
+	public NavigableMap<K, V> tailMap (
+		final K fromKey,
+		final boolean inclusive
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // tailMap()
+
+	@Override
+	public Object clone (
+	) { // method body
+		// todo
+		throw new NoSuchMethodError();
+	} // clone()
+
+	// todo
+} // AvlTreeMap
