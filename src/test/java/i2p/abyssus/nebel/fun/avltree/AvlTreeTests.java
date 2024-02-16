9cf9fc469810a0ad86a99bda6419fc9c361801e3
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-02-17 01:49:29 +0800

    Добавляем тесты метода поиска элемента

--- src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeTests.java
+++ src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeTests.java
@@ -102,5 +102,58 @@ public class AvlTreeTests {
 		Assertions.assertNull(foundItem, "In the tree not containing 2, an item with key of 2 was found");
 	} // findItemByKey_treeContains13579_notFound2()
 
+	/**
+	 * В пустом дереве не должно найтись никакого элемента.
+	 */
+	@Test
+	public void findItem_emptyTree_nothingFound (
+	) { // method body
+		// arrange
+		final Integer item = rng.nextInt();
+		// act
+		final Integer foundItem = tree.findItem(item);
+		// assert
+		Assertions.assertNull(foundItem, "In the empty tree was found an item");
+	} // findItem_emptyTree_nothingFound()
+
+	/**
+	 * Дерево содержит элементы 1, 3, 5, 7, 9. Элемент 7 должен быть найден.
+	 */
+	@Test
+	public void findItem_treeContains13579_found7 (
+	) { // method body
+		// arrange
+		tree.put(1);
+		tree.put(3);
+		tree.put(5);
+		tree.put(7);
+		tree.put(9);
+		final Integer seven = 7;
+		// act
+		final Integer foundItem = tree.findItem(seven);
+		// assert
+		Assertions.assertNotNull(foundItem, "In the tree containing 7, 7 was not found");
+		Assertions.assertEquals(seven, foundItem, "In the tree containing 7, for search request of 7, was found not 7");
+	} // findItem_treeContains357_found7()
+
+	/**
+	 * Дерево содержит элементы 1, 3, 5, 7, 9. Элемент 2 не должен быть найден.
+	 */
+	@Test
+	public void findItem_treeContains13579_notFound2 (
+	) { // method body
+		// arrange
+		tree.put(1);
+		tree.put(3);
+		tree.put(5);
+		tree.put(7);
+		tree.put(9);
+		final Integer two = 2;
+		// act
+		final Integer foundItem = tree.findItem(two);
+		// assert
+		Assertions.assertNull(foundItem, "In the tree not containing 2, 2 was found");
+	} // findItem_treeContains13579_notFound2()
+
 	// todo
 } // AvlTreeTests
