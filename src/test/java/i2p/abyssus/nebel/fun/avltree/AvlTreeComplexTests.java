c587a46bcd805158aa7117c411ca9ccd3c6fa6e2
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-04-14 11:53:11 +0800

    Добавляем тест высоты дерева, после добавления элементов

--- src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
+++ src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
@@ -1,6 +1,8 @@
 package i2p.abyssus.nebel.fun.avltree;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
@@ -44,6 +46,30 @@ public class AvlTreeComplexTests {
 	) { // method body
 		tree = new AvlTree<Long, Integer>(keyExtractor, keyComparator);
 	} // initFields()
+
+	/**
+	 * Добавление большого числа элементов.
+	 * Высота дерева не должна превышать теоретический максимум.
+	 * Ожидания: высота дерева не превышает 1.45*log2(n+2).
+	 */
+	@Test
+	public void treeHeight_afterAddingItems (
+	) { // method body
+		// arrange
+		final int MIN_N = 0x2000;
+		final int MAX_N = 0x80_0000;
+		int n = rng.nextInt(MIN_N, MAX_N + 1);
+		// act
+		for (int i = n; i > 0; i--) {
+			final Long item = rng.nextLong();
+			tree.put(item);
+		} // for
+		n = (int) tree.size();
+		// assert
+		final double maxHeight = (1.45 / Math.log(2)) * Math.log(n + 2);
+		final boolean isValidHeight = (tree.height() <= maxHeight);
+		Assertions.assertTrue(isValidHeight, "Добавление большого числа элементов. Высота превысила максимум");
+	} // treeHeight_afterAddingItems()
 
 	// todo
 } // AvlTreeComplexTests
