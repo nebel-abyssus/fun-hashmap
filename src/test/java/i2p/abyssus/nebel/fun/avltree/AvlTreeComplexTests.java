017df38d57ac1cec489644960757b0fb937056ac
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-04-18 20:17:20 +0800

    Добавляем тест удаления случайных элементов итератором

--- src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
+++ src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.Iterator;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 import java.util.random.RandomGenerator;
@@ -104,6 +103,59 @@ public class AvlTreeComplexTests {
 		final boolean isValidHeight = (tree.height() <= maxHeight);
 		Assertions.assertTrue(isValidHeight, "Удаление большого числа элементов. Высота превысила максимум");
 	} // treeHeight_afterRemovingItems()
+
+	/**
+	 * Удаление случайных элементов итератором.
+	 * Ожидания: остались все элементы кроме тех, что были удалены.
+	 */
+	@Test
+	public void iteratorRemove_removingRandomItems_allRemainingItems (
+	) { // method body
+		// arrange
+		final int MIN_N = 128;
+		final int MAX_N = 8192;
+		final int n = rng.nextInt(MIN_N, MAX_N + 1);
+		final int m = rng.nextInt((n / 4), (n / 4 * 3) + 1);
+		final NavigableMap<Integer, Long> stdTree = new TreeMap<Integer, Long>(keyComparator);
+		int virtualSize = n;
+		int virtualPos = 0;
+		// act
+		while (tree.size() != n) {
+			final Long item = rng.nextLong();
+			final Integer key = keyExtractor.apply(item);
+			stdTree.put(key, item);
+			tree.put(item);
+		} // while
+		ListIterator<Long> iterator = tree.iterator();
+		for (int i = m; i > 0; i--) {
+			boolean direction = rng.nextBoolean();
+			direction = (direction && (virtualPos != virtualSize)) || (!direction && (virtualPos == 0));
+			Long item = null;
+			if (direction) {
+				final int stepCount = rng.nextInt(virtualSize - virtualPos) + 1;
+				for (int j = stepCount; j > 0; j--) {
+					item = iterator.next();
+				} // for
+				virtualPos += stepCount - 1;
+			} else {
+				final int stepCount = rng.nextInt(virtualPos) + 1;
+				for (int j = stepCount; j > 0; j--) {
+					item = iterator.previous();
+				} // for
+				virtualPos -= stepCount;
+			} // if
+			stdTree.remove(keyExtractor.apply(item));
+			iterator.remove();
+			virtualSize--;
+		} // for
+		// assert
+		iterator = tree.iterator();
+		for (final Long expectedItem : stdTree.values()) {
+			final Long actualItem = iterator.next();
+			Assertions.assertSame(expectedItem, actualItem, "Удаление случайных элементов итератором. Сверка элементов с образцом. Элементы не совпадают");
+		} // for
+		Assertions.assertFalse(iterator.hasNext(), "Удаление случайных элементов итератором. Сверка количества элементов. В тестируемом дереве лишние элементы");
+	} // iteratorRemove_removingRandomItems_allRemainingItems()
 
 	// todo
 } // AvlTreeComplexTests
