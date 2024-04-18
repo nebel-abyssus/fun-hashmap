f427d242c0c62fe668a1319de2ca8694bf113ca8
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-04-18 10:21:18 +0800

    Добавляем тест высоты дерева, при удалении большого числа элементов
    Тест не прошёл, благодаря чему, была обнаружена бага в методе remove() итератора.

--- src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
+++ src/test/java/i2p/abyssus/nebel/fun/avltree/AvlTreeComplexTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 import java.util.random.RandomGenerator;
@@ -70,6 +71,39 @@ public class AvlTreeComplexTests {
 		final boolean isValidHeight = (tree.height() <= maxHeight);
 		Assertions.assertTrue(isValidHeight, "Добавление большого числа элементов. Высота превысила максимум");
 	} // treeHeight_afterAddingItems()
+
+	/**
+	 * Удаление большого числа элементов.
+	 * Высота дерева не должна превышать теоретический максимум.
+	 * Ожидания: высота дерева не превышает 1.45*log2(n+2).
+	 */
+	@Test
+	public void treeHeight_afterRemovingItems (
+	) { // method body
+		// arrange
+		final int REDUCED_SIZE_MIN = 0x80;
+		final int FULL_SIZE_MIN = 0x2000;
+		final int FULL_SIZE_MAX = 0x80_000;
+		final int fullSize = rng.nextInt(FULL_SIZE_MIN, FULL_SIZE_MAX + 1);
+		final int reducedSize = rng.nextInt(REDUCED_SIZE_MIN, FULL_SIZE_MIN);
+		// act
+		while (tree.size() < fullSize) {
+			final Long item = rng.nextLong();
+			tree.put(item);
+		} // while
+		final Iterator<Long> iterator = tree.iterator();
+		while (iterator.hasNext()) {
+			iterator.next();
+			final boolean doRemoving = (rng.nextInt(fullSize) >= reducedSize);
+			if (doRemoving) {
+				iterator.remove();
+			} // if
+		} // while
+		// assert
+		final double maxHeight = (1.45 / Math.log(2)) * Math.log(tree.size() + 2);
+		final boolean isValidHeight = (tree.height() <= maxHeight);
+		Assertions.assertTrue(isValidHeight, "Удаление большого числа элементов. Высота превысила максимум");
+	} // treeHeight_afterRemovingItems()
 
 	// todo
 } // AvlTreeComplexTests
