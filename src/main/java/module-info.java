f9dd3fc94209a165362c5b73acd360eb11310a7c
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-04-22 19:42:39 +0800

    Добавляем описание модуля

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Модуль содержит реализацию мапы, созданную в целях развлечения.
+ */
+module i2p.abyssus.nebel.fun.hashmap {
+
+	exports i2p.abyssus.nebel.fun.avltree;
+
+} // i2p.abyssus.nebel.fun.hashmap
