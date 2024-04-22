393e8c2093fafaa734fc7513b6b32152f25e33ea
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2024-04-22 19:21:49 +0800

    Добавляем описание пакета, содержащего реализацию дерева

---
+++ src/main/java/i2p/abyssus/nebel/fun/avltree/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Пакет содержит реализацию <a href="https://ru.wikipedia.org/wiki/%D0%90%D0%92%D0%9B-%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE">АВЛ-дерева</a>.
+ */
+package i2p.abyssus.nebel.fun.avltree;
